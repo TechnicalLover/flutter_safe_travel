04bd9edca945149b8a3aace75916de164482d0c2
Sang <sadwindsang@gmail.com>
2021-09-25 11:12:28 +0700

    do my tour details

---
+++ lib/screens/tour/details_screen/my_tour_detail.dart
@@ -0,0 +1,313 @@
+import 'package:flutter/material.dart';
+import 'package:safetravel/utilities/constants.dart';
+
+// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors_in_immutables
+// ignore_for_file: prefer_const_literals_to_create_immutables
+// ignore_for_file: public_member_api_docs
+class TravelDetailPage extends StatefulWidget {
+  @override
+  _TravelDetailPageState createState() => _TravelDetailPageState();
+}
+
+class _TravelDetailPageState extends State<TravelDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      image: DecorationImage(
+                          image: AssetImage("assets/yachts/yacht3.jpg"),
+                          fit: BoxFit.cover)),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Container(
+                            height: 32,
+                            width: 32,
+                            decoration: BoxDecoration(
+                                color: Colors.white, shape: BoxShape.circle),
+                            child: Center(
+                              child: Icon(
+                                Icons.arrow_back_ios,
+                                size: 12,
+                              ),
+                            ),
+                          ),
+                        ),
+                        left: 16,
+                        top: 16,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              flex: 15,
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(left: 16, right: 16, top: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "Resort VinPearl",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: Row(
+                        children: [
+                          Text(
+                            "🇻🇳",
+                            style: TextStyle(
+                              fontSize: 12,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            "Phu Quoc, Viet Nam",
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "⭐",
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          "4.5/5 (1250)",
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text("\$1299.00",
+                            style: TextStyle(
+                                fontSize: 14,
+                                decoration: TextDecoration.lineThrough,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold)),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "\$999.00",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: kPrimaryColor),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, "
+                      "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                      " Ut enim ad minim veniam,"
+                      " quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+                      overflow: TextOverflow.fade,
+                    )
+                  ],
+                ),
+              ),
+              flex: 7,
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Safety",
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "WiFi",
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Lunch",
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        "Spa",
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              flex: 3,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 72,
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Wrap(
+                children: [
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        color: kPrimaryColor,
+                      ),
+                      Text(
+                        "Hà Tiên",
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "---------------",
+                    style: TextStyle(color: kPrimaryColor),
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.assistant_photo,
+                        color: kPrimaryColor,
+                      ),
+                      Text(
+                        "Phú Quốc",
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Wrap(
+                    children: [
+                      Text(
+                        "Ngày khởi hành:",
+                        style: TextStyle(fontSize: 14, color: Colors.grey),
+                      ),
+                      Text(
+                        "12/05/2021",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Wrap(
+                    children: [
+                      Text(
+                        "Ngày kết thúc:",
+                        style: TextStyle(fontSize: 14, color: Colors.grey),
+                      ),
+                      Text(
+                        "18/05/2021",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
