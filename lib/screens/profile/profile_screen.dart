425732daef2f6197ca99cc94dce35c753fca97c1
ptcutican <doremonboy39@gmail.com>
2021-09-29 00:45:34 +0700

    Update profile_screen.dart
    finish_account

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:safetravel/main.dart';
+import 'package:safetravel/screens/auth/login_screen.dart';
+import 'package:safetravel/utilities/constants.dart';
 
 class ProfileScreen extends StatefulWidget {
-  ProfileScreen({Key? key}) : super(key: key);
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
@@ -11,6 +14,175 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: SafeArea(
+            child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://media.npr.org/assets/img/2021/04/27/prancer_wide-db59609b5bd96c9e56e4dfe32d198461197880c2.jpg?s=1400"),
+                        fit: BoxFit.cover)),
+                child: Container(
+                  width: double.infinity,
+                  height: 200,
+                  child: Container(
+                    alignment: Alignment(0.0, 2.5),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"),
+                      radius: 60.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Text(
+                "This is my name",
+                style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.blueGrey,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "10 years old - USA",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Im a super cute pet",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                  onPressed: () => {},
+                  color: kPrimaryColor,
+                  child: Text(
+                    "Edit Profile",
+                    style: TextStyle(letterSpacing: 2.0, color: Colors.white),
+                  )),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Activities",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black45,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w300),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            RaisedButton(
+                                onPressed: () => {},
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5)),
+                                color: kPrimaryColor,
+                                child: Text(
+                                  "MY POSTS",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                            SizedBox(
+                              height: 7,
+                            ),
+                            Text(
+                              "15",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w300),
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            RaisedButton(
+                                onPressed: () => {},
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5)),
+                                color: kPrimaryColor,
+                                child: Text(
+                                  "MY TOURS",
+                                  style: TextStyle(color: Colors.white),
+                                )),
+                            SizedBox(
+                              height: 7,
+                            ),
+                            Text(
+                              "22",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w300),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  RaisedButton(
+                      onPressed: () => {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      color: kPrimaryColor,
+                      child: Text(
+                        "About us",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                  RaisedButton(
+                      onPressed: () => {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      color: kPrimaryColor,
+                      child: Text(
+                        "Sign out",
+                        style: TextStyle(color: Colors.white),
+                      ))
+                ],
+              )
+            ],
+          ),
+        )));
   }
 }
